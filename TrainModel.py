dc29a559d3213b3a22c3642d3ae871b3d6870898
xuhaoteoh <56465408+xuhaoteoh@users.noreply.github.com>
2020-03-03 14:45:59 +0800

    Update TrainModel.py

--- TrainModel.py
+++ TrainModel.py
@@ -1,28 +1,39 @@
 from keras.preprocessing.image import ImageDataGenerator
 from Models.KerasModel import KerasModel
 from keras.callbacks import ModelCheckpoint
-import matplotlib.pyplot as plt
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from keras import backend as K
 
-K.set_image_dim_ordering('th')
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-session = tf.Session(config=config)
+K.set_image_data_format('channels_first')
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
 
-img_width, img_height = 503, 376
+img_width, img_height = 496, 369
 train_data_dir = 'Data/train'
 validation_data_dir = 'Data/validation'
-nb_train_samples = 3700
-nb_validation_samples = 3700
-nb_epoch = 30
-batch_size = 30
+nb_train_samples = 4667
+nb_validation_samples = 4667
+nb_epochs = 30
+batch_size = 32
 
 kerasModel = KerasModel()
 model = kerasModel.get_model(img_width, img_height)
 
-model.compile(loss='binary_crossentropy',
-              optimizer='rmsprop',
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
               metrics=['accuracy'])
 
 # this is the augmentation configuration we will use for training
@@ -30,7 +41,8 @@ train_datagen = ImageDataGenerator(
     rescale=1. / 255,
     shear_range=0.2,
     zoom_range=0.2,
-    horizontal_flip=True)
+    horizontal_flip=True,
+    data_format='channels_first')
 
 # this is the augmentation configuration we will use for testing:
 # only rescaling
@@ -39,9 +51,9 @@ test_datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
-    classes=['audi', 'other'],
+    classes=['Honda', 'Perodua','Proton','Toyota'],
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 class_dictionary = train_generator.class_indices
 
@@ -50,15 +62,16 @@ print('class dictionary', class_dictionary)
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_width, img_height),
-    classes=['audi', 'other'],
+    color_mode='rgb',
+    classes=['Honda', 'Perodua','Proton','Toyota'],
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 check_pointer = ModelCheckpoint(filepath='weights.hdf5', verbose=1, save_best_only=True)
 history = model.fit_generator(
     train_generator,
     steps_per_epoch=nb_train_samples/batch_size,
-    epochs=nb_epoch,
+    epochs=nb_epochs,
     validation_data=validation_generator,
     validation_steps=nb_validation_samples/batch_size,
     callbacks=[check_pointer])
